d8ed50ccd1bb5c656dbd76c4572864fbb74b120b
aniketj2002 <aniketjindal2002@gmail.com>
2023-03-26 21:20:26 +0530

    faq content

--- src/FAQ.jsx
+++ src/FAQ.jsx
@@ -1,12 +1,47 @@
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 
-function FAQ(){
-    return(
-        <div>
-            <Navbar/>
-            <Footer/>
+function FAQ() {
+  return (
+    <div>
+      <Navbar />
+      <div class="w-[80%] mx-auto mt-20 mb-20">
+      <div class="text-3xl font-bold text-center mb-10">FAQ</div>
+        <div class="font-bold text-xl mb">Q: Who can donate blood?</div>
+        <div class="text-xl text-[#575656] mb-4 ">
+          A: Anyone who is between the ages of 17 and 65 and is in good health
+          can donate blood.
         </div>
-    )
+        <div class="font-bold text-xl mb">Q: How often can I donate blood?</div>
+        <div class="text-xl text-[#575656] mb-4">A: You can donate blood every 56 days (8 weeks).</div>
+
+        <div class="font-bold text-xl mb">Q: Is it safe to donate blood?</div>
+        <div class="text-xl text-[#575656] mb-4">
+          A: Yes, it is safe to donate blood. The equipment used is sterile, and
+          a new needle is used for each donation.
+        </div>
+
+        <div class="font-bold text-xl mb">Q: What happens during the blood donation process?</div>
+        <div class="text-xl text-[#575656] mb-4">
+          A: The process is simple and consists of registration, a brief medical
+          screening, and the actual donation process. The donation itself takes
+          about 10 minutes.
+        </div>
+        <div class="font-bold text-xl mb">Q: How long does it take to donate blood?</div>
+        <div class="text-xl text-[#575656] mb-4">A: The entire process takes about 45 minutes.</div>
+        <div class="font-bold text-xl mb">Q: What should I do before donating blood?</div>
+        <div class="text-xl text-[#575656] mb-4">
+          A: Before donating blood, you should eat a nutritious meal, drink
+          plenty of water, and get a good night's rest.
+        </div>
+        <div class="font-bold text-xl mb">Q: How can I find a blood donation center near me?</div>
+        <div class="text-xl text-[#575656] mb-4">
+          A: You can search for blood donation centers near you online or
+          contact your local Red Cross or blood bank.
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 export default FAQ;
