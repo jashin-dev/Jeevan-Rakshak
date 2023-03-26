fbdca6b168b6bccc08af809e0191cffcf4af1742
LORD-MAIYOVA <amang8775@gmail.com>
2023-03-26 13:54:30 +0530

    chat css updated

--- src/Chat.js
+++ src/Chat.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import './Chat.css'
 
 import { ChatEngine, getOrCreateChat } from 'react-chat-engine'
 import Footer from './Footer'
@@ -19,31 +20,31 @@ const Chat = () => {
 		return (
 			<div>
 
-			<div>
-				<input
-					placeholder='Username'
-					value={username}
-					onChange={(e) => setUsername(e.target.value)}
-				/>
-				<button onClick={() => createDirectChat(creds)}>
-					Create 	
-				</button>
-			</div>
+				<div>
+					<input
+						placeholder='Username'
+						value={username}
+						onChange={(e) => setUsername(e.target.value)}
+					/>
+					<button onClick={() => createDirectChat(creds)}>
+						Create
+					</button>
+				</div>
 			</div>
 		)
 	}
 
 	return (
 		<div>
-	<Navbar/>
-		<ChatEngine
-			height='100vh'
-			userName='user1'
-			userSecret='123456'
-			projectID='af97511c-26a9-479d-9d20-07cbcb792618'
-			renderNewChatForm={(creds) => renderChatForm(creds)}
-		/>
-		<Footer/>
+			<Navbar />
+			<ChatEngine
+				height='100vh'
+				userName='user1'
+				userSecret='123456'
+				projectID='af97511c-26a9-479d-9d20-07cbcb792618'
+				renderNewChatForm={(creds) => renderChatForm(creds)}
+			/>
+			<Footer />
 		</div>
 	)
 }
