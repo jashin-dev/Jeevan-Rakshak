12f7d1a6ff27761b8ebe343d395b74ac297e72f6
aniketj2002 <aniketjindal2002@gmail.com>
2023-05-12 14:00:36 +0530

    customer review

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,7 +3,7 @@ function Card({ re }) {
   const source="./images/"+re.image+".png";  
 
   return (
-    <div className="p-8 max-lg:p-4 rounded-md w-[30%] border-2 text-gray-500 flex flex-col hover:shadow-md hover:scale-[.98] transition-all duration-300 shadow-lg overflow-x-hidden review" >
+    <div className="p-8 max-lg:p-4 rounded-md h-[540px] w-[30%] border-2 text-gray-500 flex flex-col hover:shadow-md hover:scale-[.98] transition-all duration-300 shadow-lg overflow-x-hidden review" >
       <div className="mx-auto my-2">
         <img
           className="object-cover rounded-full w-[80px] h-[80px] inline-block"
--- src/CustomerReview.jsx
+++ src/CustomerReview.jsx
@@ -2,22 +2,24 @@ import { useState } from "react";
 import Card from "./Components/Card";
 import data from "./data"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 function CustomerReview() {
-   
+   const [num,update_num]=useState(0);
+ 
+  
     return (
-        <div class="w-full">
+        <div class="w-full h-[700px]">
             <div class="text-4xl font-bold text-center mt-10">
                 Customer Reviews
             </div>
             <div
-                class="flex flex-wrap p-10 max-lg:p-5 justify-center gap-5 font-semibold text-md"
+                class="flex flex-wrap p-10 max-lg:p-5 justify-around font-semibold text-md "
             >
-          {
-                data?.map((review)=>{
-                  
-                    return <Card re={review}></Card>
-                })
-               }
+          
+            <button onClick={()=>update_num(num-1>0?num-1:4)}><svg className="rounded-full hover:bg-slate-200 text-black p-2 h-fit w-fit" xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="m13.707 4.707-1.414-1.414L3.586 12l8.707 8.707 1.414-1.414L6.414 12l7.293-7.293z" /><path d="m19.707 4.707-1.414-1.414L9.586 12l8.707 8.707 1.414-1.414L12.414 12l7.293-7.293z"/></svg></button>
+                <Card re={data[num]}></Card>
+                
+              <button onClick={()=>update_num(num+1<4?num+1:0)}><svg className="rounded-full hover:bg-slate-200 text-black p-2 h-fit w-fit" xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="m11.707 3.293-1.414 1.414L17.586 12l-7.293 7.293 1.414 1.414L20.414 12l-8.707-8.707z"/><path d="M5.707 3.293 4.293 4.707 11.586 12l-7.293 7.293 1.414 1.414L14.414 12 5.707 3.293z"/></svg></button>
               
             </div>
         </div>
