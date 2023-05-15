aeb69d4e5a2225899926790356cbb2b00a84a6dc
aniketj2002 <aniketjindal2002@gmail.com>
2023-05-15 16:17:38 +0530

    ui

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -38,11 +38,11 @@ function Navbar() {
   };
   return (
     <nav
-      className="w-full flex justify-between items-center p-[10px] px-[60px] lg:flex-row flex-col sticky lg:-top-[50px] rounded-lg border-b-2 bg-white opacity-90 backdrop-blur-sm z-50 -top-[180px]"
+      className="w-full flex justify-between items-center p-[10px] px-[30px] lg:flex-row flex-col sticky lg:-top-[50px] rounded-lg border-b-2 bg-white opacity-90 backdrop-blur-sm z-50 -top-[180px]"
       id="nav"
     >
       <div
-        className="flex hover:scale-105 transition-all duration-200 relative items-end w-[205px] hover:cursor-pointer"
+        className="flex hover:scale-105 transition-all duration-200 relative items-end w-[290px] hover:cursor-pointer"
         onClick={homeRouteHandler}
       >
         <img src="images/j.png" alt="" className="w-[60px]" />
@@ -50,32 +50,32 @@ function Navbar() {
           <span className="font-bold text-[#f45454]">eevan</span>Rakshak
         </div>
       </div>
-      <div className="flex justify-center sticky lg:gap-10 gap-3 text-[18px] py-2 text-[#7C7C7C] font-semibold lg:flex-row flex-col">
-        <div className="hover:text-lightBlue transition-all duration-200 hover:cursor-pointer">
+      <div className="flex justify-center sticky lg:gap-8  gap-3 text-[18px] py-2 text-[#7C7C7C] font-semibold lg:flex-row flex-col max-lg:w-full">
+        <div className="hover:text-lightBlue transition-all duration-200 hover:cursor-pointer max-lg:w-full max-lg:text-center   max-lg:hover:bg-slate-100">
           <div onClick={FAQRouteHandler}>FAQ</div>
         </div>
-        <div className="hover:text-lightBlue transition-all duration-200">
+        <div className="hover:text-lightBlue transition-all duration-200 max-lg:w-full max-lg:text-center  max-lg:hover:bg-slate-100">
           <a onClick={homeRouteHandler} href="#aboutus">
             About Us
           </a>
         </div>
-        <div className="hover:text-lightBlue transition-all duration-200 hover:cursor-pointer">
+        <div className="hover:text-lightBlue transition-all duration-200 hover:cursor-pointer max-lg:w-full max-lg:text-center  max-lg:hover:bg-slate-100">
           <div onClick={contactRouteHandler}>Contact Us</div>
         </div>
       </div>
       <div className="flex lg:gap-5 gap-3 text-[#F7F5EB] text-[18px] font-semibold">
         <button
           onClick={()=>{navigate('/profile')}}
-          className="max-lg:w-[40vw]  rounded-md  hover:scale-105 transition-all duration-200 opacity-100"
+          className="max-lg:w-[40vw]  rounded-md  hover:scale-105 transition-all duration-200 opacity-100 max-lg:hidden"
         >
           <img src="./images/user_avatar.png" className="w-[40px] rounded-full" alt="" />
         </button>
-        {/* <button
+        <button
           onClick={chatRouteHandler}
           className="max-lg:w-[40vw] px-5 py-2 rounded-md bg-[#f45454] hover:scale-105 transition-all duration-200 opacity-100"
         >
           Chat
-        </button> */}
+        </button>
         <button
           onClick={logoutRouteHandler}
           className="max-lg:w-[40vw] px-5 py-2 rounded-md bg-[#f45454] hover:scale-105 transition-all duration-200 opacity-100"
